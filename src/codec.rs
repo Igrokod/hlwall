f67d8af42894ac993bc3c53a95d4e48782e530aa
Fedcomp <aglergen@gmail.com>
2020-02-12 13:56:16 +0300

    Encode GoldSourceQuery::A2sInfoRequest

--- src/codec.rs
+++ src/codec.rs
@@ -1,8 +1,8 @@
 use anyhow::bail;
-use byteorder::{LittleEndian, ReadBytesExt};
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use bytes::BytesMut;
-use std::io::{self, Cursor, Read};
-use tokio_util::codec::Decoder;
+use std::io::{self, Cursor, Read, Write};
+use tokio_util::codec::{Decoder, Encoder};
 
 #[derive(Debug)]
 pub(crate) enum GoldSourceQuery {
@@ -17,7 +17,7 @@ macro_rules! await_bytes {
     ($read_expr:expr) => {
         match $read_expr {
             Ok(v) => v,
-            Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => return Ok(None),
+            Err(e) if e.kind() == io::ErrorKind::UnexpectedEof => return Ok(None),
             Err(e) => return Err(e.into()),
         };
     };
@@ -27,7 +27,7 @@ const SINGLE_PACKET_HEADER: i32 = -1;
 const SPLIT_PACKET_HEADER: i32 = -2;
 
 const A2S_INFO_REQUEST_HEADER: u8 = b'T';
-const A2S_INFO_REQUEST_SIGNATURE: &[u8] = b"Source Engine Query";
+const A2S_INFO_REQUEST_SIGNATURE: &[u8] = b"Source Engine Query\x00";
 
 impl Decoder for GoldSourceQueryCodec {
     type Item = GoldSourceQuery;
@@ -60,15 +60,41 @@ impl Decoder for GoldSourceQueryCodec {
     }
 }
 
+impl Encoder for GoldSourceQueryCodec {
+    type Item = GoldSourceQuery;
+    type Error = anyhow::Error;
+
+    fn encode(&mut self, item: Self::Item, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        match item {
+            GoldSourceQuery::A2sInfoRequest => {}
+            #[allow(unreachable_patterns)]
+            other => bail!("Unsupported item: {:?}", other),
+        };
+
+        let buf = vec![];
+        let mut writer = Cursor::new(buf);
+
+        let _protocol_header = writer.write_i32::<LittleEndian>(SINGLE_PACKET_HEADER)?;
+        let _request_header = writer.write_u8(A2S_INFO_REQUEST_HEADER)?;
+        let _request_signature = writer.write(A2S_INFO_REQUEST_SIGNATURE);
+
+        dst.extend_from_slice(&writer.into_inner());
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::GoldSourceQuery;
     use super::GoldSourceQueryCodec;
     use bytes::BytesMut;
-    use tokio_util::codec::Decoder;
+    use tokio_util::codec::{Decoder, Encoder};
+
+    const A2S_INFO_REQUEST: &[u8] = b"\xff\xff\xff\xffTSource Engine Query\x00";
 
     #[test]
-    fn test_empty_request() {
+    fn test_decoder_empty_request() {
         const EMPTY_REQUEST: &[u8] = b"";
         let mut codec = GoldSourceQueryCodec::default();
         let result = codec.decode(&mut BytesMut::from(EMPTY_REQUEST));
@@ -79,7 +105,7 @@ mod tests {
     }
 
     #[test]
-    fn test_invalid_header() {
+    fn test_decoder_invalid_header() {
         const INVALID_HEADER_REQUEST: &[u8] = b"\xff\xff\xff\xfa";
         let mut codec = GoldSourceQueryCodec::default();
         let result = codec.decode(&mut BytesMut::from(INVALID_HEADER_REQUEST));
@@ -87,8 +113,7 @@ mod tests {
     }
 
     #[test]
-    fn test_a2s_info_request() {
-        const A2S_INFO_REQUEST: &[u8] = b"\xff\xff\xff\xffTSource Engine Query\x00";
+    fn test_decoder_a2s_info_request() {
         let mut codec = GoldSourceQueryCodec::default();
         let result = codec
             .decode(&mut BytesMut::from(A2S_INFO_REQUEST))
@@ -103,5 +128,17 @@ mod tests {
                 other
             ),
         }
+    }
+
+    #[test]
+    fn test_encoder_a2s_info_request() {
+        let mut codec = GoldSourceQueryCodec::default();
+        let mut buf = BytesMut::new();
+
+        codec
+            .encode(GoldSourceQuery::A2sInfoRequest, &mut buf)
+            .expect("Failed to encode A2S_INFO_REQUEST");
+
+        assert_eq!(buf, A2S_INFO_REQUEST);
     }
 }
